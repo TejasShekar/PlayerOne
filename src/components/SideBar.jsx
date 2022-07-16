1f07d72b58e39cd7e8f93e5704a64d8acfaf0177
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-07-16 19:29:24 +0530

    add aside bar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,12 +8,16 @@ export const SideBar = () => {
     backgroundColor: "#eea53f",
   };
   return (
+    // Add a btn that will toggle the side bar open and close
+    // Add icons to each route. These icons will be visible irrespective of sidebar open or close
     <aside className="flex flex-col text-xl font-normal sticky h-screen top-20 overflow-y-auto">
       <div>
         <div className="flex items-center mb-4">
           <figure className="w-14 mx-2">
             <img
               className="rounded-[50%] border-[1px] border-black"
+              // http://xsgames.co/randomusers/assets/avatars/pixel/${random_number}.jpg -> use this link to get an random number between 0 and 53 and assign that img as profile picture to a new user.
+              // src="https://xsgames.co/randomusers/avatar.php?g=pixel"
               src={fallback_DP}
               alt="profile_img"
             />
@@ -68,6 +72,8 @@ export const SideBar = () => {
           </li>
         </ol>
       </div>
+      {/* Change this to btn so that it will redirect to homepage or the previous public route while also logging out.
+      For that rewatch react router part 2*/}
       <NavLink to="/" className=" inline-block py-4 px-2 w-full ">
         Log Out
       </NavLink>
