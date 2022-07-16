271aeb468836a24b17dc3f865289082d4b0d5e79
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-07-16 19:37:40 +0530

    minor fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const NavBar = () => {
   return (
-    <div className=" w-full h-20 border-b-2 border-white fixed">
+    <div className=" w-full h-20 border-b-2 border-white sticky top-0">
       <h1 className=" font-['Press_Start_2P'] p-6 text-[1.3rem] ">PLAYER ONE</h1>
     </div>
   );
