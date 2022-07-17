a364c98541cb42cc9ea8627fcf1717f134618d83
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-07-17 14:22:58 +0530

    fix: active styles toggling issue

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,15 +2,12 @@ import {NavLink} from "react-router-dom";
 import fallback_DP from "../assets/fallback_DP.jpg";
 
 export const SideBar = () => {
-  const activeStyle = {
-    color: "black",
-    fontWeight: "700",
-    backgroundColor: "#eea53f",
-  };
+  const activeStyle = "text-black font-bold bg-[#eea53f]";
+
   return (
     // Add a btn that will toggle the side bar open and close
     // Add icons to each route. These icons will be visible irrespective of sidebar open or close
-    <aside className="flex flex-col text-xl font-normal sticky h-[calc(100vh-6rem)] top-24 overflow-y-auto">
+    <aside className="flex flex-col text-lg font-normal sticky h-[calc(100vh-6rem)] top-24 overflow-y-auto">
       <div>
         <div className="flex items-center mb-4">
           <figure className="w-14 mx-2">
@@ -28,46 +25,55 @@ export const SideBar = () => {
           <li>
             <NavLink
               to="/explore"
-              style={({isActive}) => (isActive ? activeStyle : undefined)}
-              className=" inline-block py-4 px-2 w-full "
+              className={({isActive}) =>
+                `${isActive ? activeStyle : ""} py-4 px-4 flex center`
+              }
             >
+              <span className="material-icons-outlined mr-2">explore</span>
               Explore
             </NavLink>
           </li>
           <li>
             <NavLink
               to="/playlist"
-              className={(isActive) =>
-                `${isActive ? activeStyle : ""} inline-block py-4 px-2 w-full`
+              className={({isActive}) =>
+                `${isActive ? activeStyle : ""} py-4 px-4 flex center`
               }
             >
+              <span className="material-icons-outlined mr-2">playlist_play</span>
               Playlist
             </NavLink>
           </li>
           <li>
             <NavLink
               to="/watch-later"
-              style={({isActive}) => (isActive ? activeStyle : undefined)}
-              className=" inline-block py-4 px-2 w-full "
+              className={({isActive}) =>
+                `${isActive ? activeStyle : ""} py-4 px-4 flex center`
+              }
             >
+              <span className="material-icons-outlined mr-2">watch_later</span>
               Watch Later
             </NavLink>
           </li>
           <li>
             <NavLink
               to="/liked"
-              style={({isActive}) => (isActive ? activeStyle : undefined)}
-              className=" inline-block py-4 px-2 w-full "
+              className={({isActive}) =>
+                `${isActive ? activeStyle : ""} py-4 px-4 flex center`
+              }
             >
+              <span className="material-icons-outlined mr-2">thumb_up</span>
               Liked Videos
             </NavLink>
           </li>
           <li>
             <NavLink
               to="/history"
-              style={({isActive}) => (isActive ? activeStyle : undefined)}
-              className=" inline-block py-4 px-2 w-full "
+              className={({isActive}) =>
+                `${isActive ? activeStyle : ""} py-4 px-4 flex center`
+              }
             >
+              <span className="material-icons-outlined mr-2">history</span>
               History
             </NavLink>
           </li>
@@ -75,9 +81,9 @@ export const SideBar = () => {
       </div>
       {/* Change this to btn so that it will redirect to homepage or the previous public route while also logging out.
       For that rewatch react router part 2*/}
-      <NavLink to="/" className=" inline-block py-4 px-2 w-full ">
+      {/* <NavLink to="/" className=" py-4 px-2 flex center ">
         Log Out
-      </NavLink>
+      </NavLink> */}
     </aside>
   );
 };
