3a0e81c70d4997668692f6e28334d7811a223266
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-07-17 20:25:46 +0530

    feat: simple landing page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,37 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import fallback_DP from "../assets/fallback_DP.jpg";
 
 export const NavBar = ({theme}) => {
   const {darkTheme, setDarkTheme} = theme;
 
   return (
-    <header className="flex justify-between px-4 w-full h-20 sticky top-0 border-b-2 border-[#eea53f] text-black dark:text-white bg-white dark:bg-[#252525] z-10">
+    <header className="flex justify-between p-4 w-full h-20 sticky top-0 border-b-2 border-[#eea53f] text-black dark:text-white bg-[#f9f9f9] dark:bg-[#252525] z-10">
       <Link to="/" className=" font-['Press_Start_2P'] my-auto text-[1.3rem] ">
-        PLAYER ONE
+        PLAYER
+        <span className="text-[#eea53f] font-['Press_Start_2P']">ONE</span>
       </Link>
-      <button
-        onClick={(e) => {
-          e.stopPropagation();
-          setDarkTheme(!darkTheme);
-        }}
-      >
-        <span className="material-icons-outlined text-3xl">
-          {darkTheme ? "dark_mode" : "light_mode"}
-        </span>
-      </button>
+      <div className="flex items-center gap-4">
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            setDarkTheme(!darkTheme);
+          }}
+        >
+          <span className="material-icons-outlined text-3xl">
+            {darkTheme ? "light_mode" : "dark_mode"}
+          </span>
+        </button>
+        <div className="flex items-center cursor-pointer">
+          <figure className="w-14 mx-2">
+            <img
+              className="rounded-[50%] border-[1px] border-black"
+              src={fallback_DP}
+              alt="profile_img"
+            />
+          </figure>
+        </div>
+      </div>
     </header>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,20 @@
+import {Link} from "react-router-dom";
+
 export const HomePage = () => {
   return (
-    <main>
-      <h1 className=" font-['Press_Start_2P'] text-[1.3rem] text-center">
-        WELCOME TO PLAYER ONE
-      </h1>
+    <main className="dark:bg-[#252525] dark:text-white h-[calc(100vh-5rem)] grid place-content-center">
+      <div className="flex flex-col items-center w-2/3 mx-auto">
+        <h1 className=" font-['Press_Start_2P'] text-2xl text-center leading-10">
+          THE ULTIMATE COLLECTION OF HANDFULLY SELECTED CONTENT ON GAMEPLAYS AND
+          MORE
+        </h1>
+        <Link
+          to="/explore"
+          className="m-6 p-2 px-4 font-bold text-black text-xl bg-[#eea53f] rounded-full"
+        >
+          Explore Videos
+        </Link>
+      </div>
     </main>
   );
 };
