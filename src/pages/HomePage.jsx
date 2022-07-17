49176699073c2ff2ba92f6d7a15371a95888a9f4
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-07-17 20:31:26 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,8 +5,7 @@ export const HomePage = () => {
     <main className="dark:bg-[#252525] dark:text-white h-[calc(100vh-5rem)] grid place-content-center">
       <div className="flex flex-col items-center w-2/3 mx-auto">
         <h1 className=" font-['Press_Start_2P'] text-2xl text-center leading-10">
-          THE ULTIMATE COLLECTION OF HANDFULLY SELECTED CONTENT ON GAMEPLAYS AND
-          MORE
+          THE <span className="text-[#eea53f] font-['Press_Start_2P']">ULTIMATE </span>
           COLLECTION OF HANDFULLY SELECTED CONTENT ON GAMEPLAYS AND MORE
         </h1>
         <Link
