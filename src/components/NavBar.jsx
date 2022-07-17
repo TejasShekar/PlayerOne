5006213aecef7d164db0f049b66cb234913de51d
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-07-17 14:22:20 +0530

    feat: add clickable logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export const NavBar = ({theme}) => {
   const {darkTheme, setDarkTheme} = theme;
 
   return (
-    <header className="flex justify-between px-6 w-full h-20 sticky top-0 border-b-2 border-[#eea53f] text-black dark:text-white bg-white dark:bg-[#252525]">
-      <h1 className=" font-['Press_Start_2P'] my-auto text-[1.3rem] ">PLAYER ONE</h1>
-      <button onClick={() => setDarkTheme(!darkTheme)}>
+    <header className="flex justify-between px-4 w-full h-20 sticky top-0 border-b-2 border-[#eea53f] text-black dark:text-white bg-white dark:bg-[#252525] z-10">
+      <Link to="/" className=" font-['Press_Start_2P'] my-auto text-[1.3rem] ">
+        PLAYER ONE
+      </Link>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          setDarkTheme(!darkTheme);
+        }}
+      >
         <span className="material-icons-outlined text-3xl">
           {darkTheme ? "dark_mode" : "light_mode"}
         </span>
