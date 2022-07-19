a2c46f03cd5b1c0f6bcae6091133dc8bcd63e590
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-07-19 14:51:10 +0530

    Update categories.js

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -2,26 +2,18 @@ import {v4 as uuid} from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    category: "action",
-    src: "https://raw.githubusercontent.com/TejasShekar/P8-Games/dev/src/assets/action.webp",
-    name: "Action Games",
+    name: "Action",
   },
   {
     _id: uuid(),
-    category: "adventure",
-    src: "https://raw.githubusercontent.com/TejasShekar/P8-Games/dev/src/assets/adventure.webp",
-    name: "Adventure Games",
+    name: "Adventure",
   },
   {
     _id: uuid(),
-    category: "racing",
-    src: "https://raw.githubusercontent.com/TejasShekar/P8-Games/dev/src/assets/racing.webp",
-    name: "Racing Games",
+    name: "Racing",
   },
   {
     _id: uuid(),
-    category: "sports",
-    src: "https://github.com/TejasShekar/P8-Games/blob/dev/src/assets/sports.webp",
-    name: "Sports Games",
+    name: "Sports",
   },
 ];
