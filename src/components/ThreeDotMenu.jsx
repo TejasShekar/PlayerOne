76b21ac5c37ec3341d0f017404187973f2ea29e1
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-07-22 16:33:22 +0530

    fix: threedot menu not working

--- src/components/ThreeDotMenu.jsx
+++ src/components/ThreeDotMenu.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 import {useSelector, useDispatch} from "react-redux/es/exports";
 import {addToWatchLater, removeFromWatchLater} from "../redux/features/watchLaterSlice";
-
-const isVideoInWatchLater = (id, watchLater) =>
-  watchLater.find((video) => video._id === id);
+import {isVideoInWatchLater} from "../utils/isVideoInWatchLater";
 
 export const ThreeDotMenu = ({data}) => {
-  const {videosData} = useSelector((state) => state.watchLater);
+  const {watchLaterVideos} = useSelector((state) => state.watchLater);
   const dispatch = useDispatch();
   return (
     <div className="flex flex-col gap-4 text-md my-auto bg-[#f9f9f9] dark:bg-black absolute right-6 shadow-sm shadow-black rounded p-2">
       <button className="flex center">
         <span className="material-icons-outlined mr-2">playlist_add</span>Add to Playlist
       </button>
-      {isVideoInWatchLater(data._id, videosData) ? (
+      {isVideoInWatchLater(data._id, watchLaterVideos) ? (
         <button
           className="flex center"
-          onClick={() => dispatch(removeFromWatchLater(data._id, videosData))}
+          onClick={() => dispatch(removeFromWatchLater(data._id, watchLaterVideos))}
         >
-          <span className="material-icons-outlined mr-2">delete</span>Remove from Watch
-          Later
+          <span className="material-icons mr-2">watch_later</span>Remove from Watch Later
         </button>
       ) : (
         <button className="flex center" onClick={() => dispatch(addToWatchLater(data))}>
