88a36eecf5cadbc247f977f61f7a9f9743618ccf
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-08-19 20:51:56 +0530

    add: playlist helper function

--- src/utils/videoActionHelps.js
+++ src/utils/videoActionHelps.js
@@ -5,3 +5,6 @@ export const isVideoInHistory = (id, history) => history.find(({_id}) => _id ===
 
 export const isVideoInLikedVideos = (id, likedVideos) =>
   likedVideos.find(({_id}) => _id === id);
+
+export const isVideoInPlaylist = (playlists, playlistId, videoId) =>
+  playlists.find(({_id}) => _id === playlistId)?.videos.some(({_id}) => _id === videoId);
