24d6e8bc0542fb1c3f28b96e6decd54a4f2e6309
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2024-01-06 14:31:22 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
-import {Link} from "react-router-dom";
-import {Categories} from "../components/Categories";
-import {useDocumentTitle} from "../hooks/useDocumentTitle";
+import { Link } from "react-router-dom";
+import { Categories } from "../components/Categories";
+import { useDocumentTitle } from "../hooks/useDocumentTitle";
 
 export const HomePage = () => {
   useDocumentTitle("PLAYERONE");
@@ -9,7 +9,10 @@ export const HomePage = () => {
     <main className="dark:bg-[#252525] dark:text-white h-[calc(100vh-5rem)] grid place-content-center">
       <div className="flex flex-col items-center w-2/3 mx-auto">
         <h1 className=" font-['Press_Start_2P'] text-2xl text-center leading-10">
-          THE <span className="text-[#eea53f] font-['Press_Start_2P']">ULTIMATE </span>
+          THE{" "}
+          <span className="text-[#eea53f] font-['Press_Start_2P']">
+            ULTIMATE{" "}
+          </span>
           COLLECTION OF HANDFULLY SELECTED CONTENT ON GAMEPLAYS AND MORE
         </h1>
         <Link
