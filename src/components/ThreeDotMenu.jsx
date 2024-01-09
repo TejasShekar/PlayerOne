30a4251e33f354a8db0a2aa25fd53ea16ca719c4
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2024-01-09 17:41:09 +0530

    refactor menu component

--- src/components/ThreeDotMenu.jsx
+++ src/components/ThreeDotMenu.jsx
@@ -1,26 +1,22 @@
 import React from "react";
-import {useSelector, useDispatch} from "react-redux/es/exports";
-import {useLocation, useNavigate, useParams} from "react-router-dom";
-import {removeFromHistory} from "../redux/features/historySlice";
-import {removeFromLikedVideos} from "../redux/features/likedSlice";
-import {
-  removePlaylist,
-  removeVideoFromPlaylist,
-  setIsModalOpen,
-} from "../redux/features/playlistSlice";
-import {addToWatchLater, removeFromWatchLater} from "../redux/features/watchLaterSlice";
-import {isVideoInWatchLater} from "../utils/videoActionHelps";
-import {PlaylistModal} from "./PlaylistModal";
+import { useSelector, useDispatch } from "react-redux/es/exports";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { removeFromHistory } from "../redux/features/historySlice";
+import { removeFromLikedVideos } from "../redux/features/likedSlice";
+import { removeVideoFromPlaylist, setIsModalOpen } from "../redux/features/playlistSlice";
+import { addToWatchLater, removeFromWatchLater } from "../redux/features/watchLaterSlice";
+import { isVideoInWatchLater } from "../utils/videoActionHelps";
+import { PlaylistModal } from "./PlaylistModal";
 
-export const ThreeDotMenu = ({data}) => {
-  const {watchLaterVideos} = useSelector((state) => state.watchLater);
-  const {token} = useSelector((state) => state.auth);
+export const ThreeDotMenu = ({ data }) => {
+  const { watchLaterVideos } = useSelector((state) => state.watchLater);
+  const { token } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const foundInWatchLater = isVideoInWatchLater(data._id, watchLaterVideos);
-  const {pathname} = useLocation();
-  const {isModalOpen} = useSelector((state) => state.playlist);
-  const {playlistId} = useParams();
+  const { pathname } = useLocation();
+  const { isModalOpen } = useSelector((state) => state.playlist);
+  const { playlistId } = useParams();
 
   return (
     <div className="flex flex-col gap-4 text-md my-auto bg-[#f9f9f9] dark:bg-black absolute right-6 top-0 shadow-sm shadow-black rounded p-2">
@@ -82,7 +78,7 @@ export const ThreeDotMenu = ({data}) => {
           onClick={(e) => {
             e.stopPropagation();
             const videoId = data._id;
-            token && dispatch(removeVideoFromPlaylist({videoId, playlistId}));
+            token && dispatch(removeVideoFromPlaylist({ videoId, playlistId }));
           }}
         >
           <span className="material-icons-outlined mr-2">block</span>Remove from current
